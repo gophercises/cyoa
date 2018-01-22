190739b581e31fd1f5c72550f0238c7ff2928a54
Fkady <b4q.2tq@gmail.com>
2018-01-22 17:54:34 +0900

    add http handler

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -5,9 +5,12 @@ import (
 	"flag"
 	"os"
 	"github.com/Fkady/gophercises/cyoa"
+	"log"
+	"net/http"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start CYOA application on")
 	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story.")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -22,5 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- story.go
+++ story.go
@@ -3,7 +3,30 @@ package cyoa
 import (
 	"io"
 	"encoding/json"
+	"net/http"
+	"html/template"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
@@ -27,3 +50,29 @@ type Option struct {
 	Text string `json:"text"`
 	Chapter  string `json:"arc"`
 }
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      {{if .Options}}
+        <ul>
+        {{range .Options}}
+          <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+      {{else}}
+        <h3>The End</h3>
+      {{end}}
+    </section>
+  </body>
+</html>`
