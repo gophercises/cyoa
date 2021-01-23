1cf49672a4bc86fe4facf616de50d52c67341d70
mova <valeriy.molchanov.77@gmail.com>
2021-01-23 11:22:55 +0200

    WithPathFunc

--- students/movaua/pkg/handler/handler.go
+++ students/movaua/pkg/handler/handler.go
@@ -32,17 +32,33 @@ func WithTemplate(t *template.Template) Option {
 	}
 }
 
+// WithPathFunc sets the provided path function
+func WithPathFunc(fn PathFn) Option {
+	return func(h *Handler) {
+		h.pathFn = fn
+	}
+}
+
+// PathFn is function that returns a path for request
+type PathFn func(r *http.Request) string
+
+func defaultPath(r *http.Request) string {
+	return strings.TrimPrefix(strings.TrimSpace(r.URL.Path), "/")
+}
+
 // Handler is CYOA Story http.Handler
 type Handler struct {
-	story model.Story
-	tpl   *template.Template
+	story  model.Story
+	tpl    *template.Template
+	pathFn PathFn
 }
 
 // New creates new handler for provided Story
 func New(story model.Story, opts ...Option) http.Handler {
 	h := &Handler{
-		story: story,
-		tpl:   defaultTpl,
+		story:  story,
+		tpl:    defaultTpl,
+		pathFn: defaultPath,
 	}
 
 	for _, opt := range opts {
@@ -53,7 +69,7 @@ func New(story model.Story, opts ...Option) http.Handler {
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := strings.TrimPrefix(strings.TrimSpace(r.URL.Path), "/")
+	path := h.pathFn(r)
 
 	if path == "" {
 		http.Redirect(w, r, fmt.Sprintf("/%s", intro), http.StatusFound)
